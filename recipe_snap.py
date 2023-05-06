27c3d585edc488350476ebcf952d6e6670dc197d
Kevin Capano <81310090+capanokevin@users.noreply.github.com>
2023-05-06 17:15:13 +0200

    Update recipe_snap.py

--- recipe_snap.py
+++ recipe_snap.py
@@ -11,9 +11,8 @@ from collections import defaultdict
 from recipe_encoder import * 
 from image_encoder import *
 from HydraNet import *
-#from utils.preprocessing import get_image_loader, get_recipe_loader, recipe_preprocessing 
-#from utils.utils import load_checkpoint, count_parameters
-from utils import get_image_loader, get_recipe_loader, recipe_preprocessing , load_checkpoint, count_parameters
+from utils.preprocessing import get_image_loader, get_recipe_loader, recipe_preprocessing 
+from utils.utils import load_checkpoint, count_parameters
 import shutil
 from torchvision import transforms
 
